d6327eb5ef644b209b31d48eee452c3fae6daab7
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2019-12-20 18:18:11 +0100

    pushIn Effect added

--- README.md
+++ README.md
@@ -1,13 +1,53 @@
-# Landing Page Project
-
 ## Table of Contents
 
-* [Instructions](#instructions)
+1. [Project Description](#description)
+3. [Project Components](#components)
+4. [Instructions](#instructions)
+5. [Licensing, Authors, and Acknowledgements](#licensing)
 
-## Instructions
+## Project Description <a name="description"></a>
+
 
 The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+## Project Components <a name="components"></a>
+
+Main components of this project are the images, HTML, and CSS files used to design
+the Website's Front End. There are two HTML files related to:
+<ul>
+<li> The Blog Website: index.html </li>
+<li> an example Blog Post: blog-post.html </li>
+</ul>
+As well as the related CSS files for responsive layout and styling:
+
+<ul>
+<li> layout.css </li>
+<li> style.css </li>
+<li> blog-post.css </li>
+<li> post-style.css </li>
+</ul>
+
+## Instructions<a name="instructions"></a>
+
+From the main blog website, click on <strong>Read More</strong> and the blog Post
+template will show up, from there you can come back to the main page by clicking
+on the <strong>HOME</strong> button.
+
+## Licensing, Authors, Acknowledgements<a name="licensing"></a>
+
+<strong><em>Author:<em></strong> `Gustavo Cedeno` following recommendations and requirements from Udacity's
+Front End Developer NanoDegree Program.
+For authors and further information on the "Smooth Scroll and pushIn effects" check the references below.
+
+Main Sources of Reference:
+* [Udacity's Front End Developer ND](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011)
+* [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Learn)
+* [W3C UI Events](https://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
+* [Smooth Scroll](https://github.com/cferdinandi/smooth-scroll#global-settings)
+* [pushIn Effect](https://github.com/nateplusplus/pushIn.js)
+# FEND: Landing Page Project
--- js/app.js
+++ js/app.js
@@ -13,26 +13,75 @@
  *
 */
 //defining event listeners
-window.addEventListener('scroll', highlight);
+// window.addEventListener('scroll', highlight);
+window.addEventListener("scroll", (event) => {
+    highlight();
+    updateDistinguishedSection();
+    checkForScrollButton();
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    buildNav();
+    // scrollSmoothly();
+});
 /**
  * Define Global Variables
  *
 */
-//defining variables and arrays
 let navList = document.querySelector('#navbar__list');
-let sectionList = document.querySelectorAll('section');
+let sections = document.querySelectorAll('section');
 let scrollBack = document.querySelector('.scrollTop');
 let nameListItem = [];
-let sectionCollection = [];
+let sectionList = [];
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
+/**********************************************/
+function isElementInViewport(el){
+    let rect = el.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
 
+    // Only completely visible elements return true:
+    const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    // const isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
 
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
 
+function checkForScrollButton(){
+    // Get the current scroll value
+    let y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 0) {
+        scrollToTopButton.className = "show";
+      }
+    else {
+        scrollToTopButton.className = "hide";
+    }
+}
+
+// Add class 'active' to section when near top of viewport
+function updateDistinguishedSection(){
+    for (const landingPgSection of landingPgSections){
+        if (isElementInViewport(landingPgSection)){
+            landingPgSection.classList.add("your-active-class");
+        }
+        else {
+            landingPgSection.classList.remove("your-active-class");
+        }
+    }
+};
+/**********************************************/
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -40,18 +89,12 @@ let sectionCollection = [];
 */
 
 // build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
 function buildNav() {
-  for (let i = 0; i < sectionList.length; i++) {
+  for (let i = 0; i < sections.length; i++) {
 // variables here are used to get the (1)text content for nav items and (2)id for anchors, also (3)added each section to Array for further use:
-    let itemText = sectionList[i].getAttribute('data-nav');
-    let itemId = sectionList[i].getAttribute('id');
-    sectionCollection[i] = sectionList[i];
+    let itemText = sections[i].getAttribute('data-nav');
+    let itemId = sections[i].getAttribute('id');
+    sectionList[i] = sections[i];
 //creating (1)list items and (2)anchors and (3)setting the value, got from upper variable as href value:
     nameListItem[i] = document.createElement('li');
     let nameListItemA = document.createElement('a');
@@ -65,34 +108,37 @@ function buildNav() {
   }
 }
 
-buildNav();
+// buildNav();
 
+// Add class 'active' to section when near top of viewport
 // function changes styling of sections when active
 function highlight (e) {
 //loop through section list stored in an array sectionCollection;
-  for(let i = 0; i < sectionCollection.length; i++){
+  for(let i = 0; i < sectionList.length; i++){
 //checks if section has active class and saves value as a boolian:
-    let containsActiveClass = sectionCollection[i].classList.contains('your-active-class');
+    let containsActiveClass = sectionList[i].classList.contains('your-active-class');
 //changes active class based on position of user screen view:
     if (!containsActiveClass) {
-       if(window.scrollY >= sectionCollection[i].offsetTop - 150 && window.scrollY <= sectionCollection[i].offsetTop + 500) {
-        sectionCollection[i].className = 'your-active-class';
+       if(window.scrollY >= sectionList[i].offsetTop - 150 && window.scrollY <= sectionList[i].offsetTop + 500) {
+        sectionList[i].className = 'your-active-class';
         nameListItem[i].className = 'link_active';
       }
     }else {
-      if(window.scrollY < sectionCollection[i].offsetTop - 150 || window.scrollY > sectionCollection[i].offsetTop + 500) {
-       sectionCollection[i].classList.remove('your-active-class');
+      if(window.scrollY < sectionList[i].offsetTop - 150 || window.scrollY > sectionList[i].offsetTop + 500) {
+       sectionList[i].classList.remove('your-active-class');
        nameListItem[i].classList.remove('link_active');
      }
     }
   }
+
+// Scroll to anchor ID using scrollTO event
 //  test where the user is located and inserting "back to top" button to the screen
-  if( window.scrollY > document.querySelector('main').offsetHeight - 800) {
+  if( window.scrollY > document.querySelector('main').offsetHeight - 1800) {
     scrollBack.style.display = 'block';
   }else {
     scrollBack.style.display = 'none';
   }
-}
+};
 /**
  * End Main Functions
  * Begin Events
@@ -106,17 +152,14 @@ function highlight (e) {
 // Set sections as active
 // added code from https://github.com/cferdinandi/smooth-scroll to get a scroll effect
 // const scroll = new SmoothScroll('a[href*="#"]', {
-// 	speed: 600
 // });
 const scroll = new SmoothScroll('a[href*="#"]', {
-	// Function. Custom easing pattern
-	// If this is set to anything other than null, will override the easing option above
+  speed:500,
+  speedAsDuration: false,
+  // Function. Custom easing pattern
 	customEasing: function (time) {
 
 		// return <your formulate with time as a multiplier>
-
-		// Example: easeInOut Quad
 		return time < 0.2 ? 10 * time * time : -1 + (4 - 2 * time) * time;
-
 	}
 });
